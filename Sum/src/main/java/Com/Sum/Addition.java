89e77bb05ab9318f8e28f08b52cc1e51ca86b909
Rashmi Joshi <rashmi.joshi@nihilent.com>
2020-02-14 13:19:46 +0530

    first commit

---
+++ Sum/src/main/java/Com/Sum/Addition.java
@@ -0,0 +1,13 @@
+package Com.Sum;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=2 ;
+		int b=3;
+		System.out.print("Addition is :" +(a+b));
+	}
+
+}
